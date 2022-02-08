dcc67f13bf265f27ba62b458a3bb7e2e40513a55
bnewton <bnewton@mac.com>
2022-02-07 22:05:55 -0600

    Initial Commit

---
+++ PaymentCalculator/PaymentCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentCalculatorApp.swift
+//  PaymentCalculator
+//
+//  Created by bnewton on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PaymentCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
