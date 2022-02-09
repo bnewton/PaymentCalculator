c908c833123867596329a4b69f93f05205282fe2
bnewton <bnewton@mac.com>
2022-02-08 22:03:57 -0600

    Update ContentView.swift

--- PaymentCalculator/ContentView.swift
+++ PaymentCalculator/ContentView.swift
@@ -8,14 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @FocusState private var vehicleSalesPriceIsFocused: Bool
+    @FocusState private var cashDownPaymentIsFocused: Bool
+
+    
+    @State private var vehicleSalesPrice = 15_000.00
+    @State private var interestRate = 6
+    @State private var loanTerm = 60
+    @State private var cashDownPayment = 0.00
+    
+    var amountFinanced: Double {
+        let total = vehicleSalesPrice - cashDownPayment
+        
+        return total
+    }
+    
+    var monthlyPayment: Double {
+        
+        let principal = amountFinanced
+        let rate = (Double(interestRate) / 100) / 12
+        
+        let x = (rate * pow(1.00 + rate, Double(loanTerm))) / (pow(1.00 + rate, Double(loanTerm)) - 1)
+
+        return Double(principal * x)
+    }
+    
+    let loanTerms = [24, 36, 39, 42, 48, 60, 66, 72, 84]
+    
+    var currencyFormat: FloatingPointFormatStyle<Double>.Currency{
+        let currencyCode = Locale.current.currencyCode ?? "USD"
+        return FloatingPointFormatStyle<Double>.Currency(code: currencyCode)
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Vehicle Sales Price", value: $vehicleSalesPrice, format: currencyFormat)
+                        .keyboardType(.decimalPad)
+                        .focused($vehicleSalesPriceIsFocused)
+                        
+                    Picker("Interest Rate", selection: $interestRate){
+                        ForEach(0..<11){
+                            Text("\($0)%")
+                        }
+                    }
+                } header: {
+                    Text("Vehicle Sales Price")
+                }
+                
+              
+                Section {
+                    Picker("Loan Term", selection: $loanTerm){
+                        ForEach(loanTerms, id: \.self){
+                            Text("\($0) months")
+                        }
+                    }
+                }
+                
+                Section {
+                    TextField("Cash Down Payment", value: $cashDownPayment, format: currencyFormat)
+                        .keyboardType(.decimalPad)
+                        .focused($cashDownPaymentIsFocused)
+
+
+                }header: {
+                    Text("Cash Down Payment")
+                }
+
+                Section {
+                    Text(amountFinanced, format: currencyFormat)
+                } header: {
+                    Text("Amount financed")
+                }
+                
+                Section {
+                    Text(monthlyPayment, format: currencyFormat)
+                } header: {
+                    Text("Estimated monthly car payment")
+                }
+                
+                
+            }.navigationTitle("Payment Calculator")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard){
+                        Spacer()
+                        Button("Done") {
+                            
+                        
+                            vehicleSalesPriceIsFocused = false
+                            cashDownPaymentIsFocused = false
+                        }
+                    }
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ZStack{
+            ContentView()
+        }
+        
     }
 }
